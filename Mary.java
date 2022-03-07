767593cf18723affddeba40d39e43ca854bf645b
MaryOs2306 <al917703@gmail.com>
2022-03-07 10:16:18 +0200

    chaged for the third time

--- Mary.java
+++ Mary.java
@@ -1,7 +1,7 @@
 public class Mary{
 	public static void main(String[] args){
 		for(int i=1;i<=5;i++){
-			System.out.println("Mary Ostrovsky "+i);
+			System.out.println("Mary Ostrovsky... "+i);
 		}
 	}
 }
